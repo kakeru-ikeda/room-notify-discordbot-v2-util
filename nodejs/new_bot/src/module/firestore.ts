b127a3285cea8e2874562b23a459f846ab288c1a
kakeru-ikeda <kakerumail1451@gmail.com>
2024-02-12 17:18:48 +0900

    Firestoreの秘密鍵はenvで一括管理

--- nodejs/new_bot/src/module/firestore.ts
+++ nodejs/new_bot/src/module/firestore.ts
@@ -1,8 +1,16 @@
+import * as dotenv from 'dotenv'
 import admin from 'firebase-admin';
-import ServiceAccount from '../../serviceAccount.json';
+
+dotenv.config();
 
 admin.initializeApp({
-    credential: admin.credential.cert(ServiceAccount as admin.ServiceAccount),
+    credential: admin.credential.cert(
+        {
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n')
+        }
+    ),
 });
 
 export const db = admin.firestore();
