fb6c23097439824306f727ab38e26c82678a9e66
kakeru-ikeda <kakerumail1451@gmail.com>
2024-02-19 00:48:28 +0900

    旧Botは削除

--- nodejs/bot/module/bot.js
+++
@@ -1,3 +0,0 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-
-module.exports.client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers] });
